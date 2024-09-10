07b943076de802b3884908ca8892cf41ef285741
James Bartlett <james.bartlett@glasgow.ac.uk>
2024-09-10 23:02:37 +0100

    Push initial changes

---
+++ include/deadlines.R
@@ -0,0 +1,17 @@
+study_year <- "2024 / 2025"
+
+groupagreement_due <- "October 18th 2024"
+
+stage1_due <- "November 15th 2024"
+
+stage1_words <- 1500
+
+stage2_due <- "December 13th 2024"
+
+stage2_words <- 1500
+
+MCQ_due <- "October 25th 2024"
+
+dataskills1_due <- "November 8th 2024"
+
+dataskills2_due <- "November 29th 2024"
