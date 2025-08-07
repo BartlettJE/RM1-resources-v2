0f176f69f16ecdde47f4581afff90e6e55e798f2
James Bartlett <james.bartlett@glasgow.ac.uk>
2025-08-07 12:36:37 +0100

    Update pages to 25/26 deadlines

--- include/deadlines.R
+++ include/deadlines.R
@@ -1,17 +1,17 @@
 study_year <- "2025 / 2026"
 
-groupagreement_due <- "October 11th 2024"
+groupagreement_due <- "October 10th 2025"
 
-stage1_due <- "November 15th 2024"
+stage1_due <- "November 14th 2025"
 
 stage1_words <- 1500
 
-stage2_due <- "December 13th 2024"
+stage2_due <- "December 12th 2025"
 
 stage2_words <- 1500
 
-MCQ_due <- "October 25th 2024"
+MCQ_due <- "October 24th 2025"
 
-dataskills1_due <- "November 8th 2024"
+dataskills1_due <- "November 7th 2025"
 
-dataskills2_due <- "November 29th 2024"
+dataskills2_due <- "November 28th 2025"
